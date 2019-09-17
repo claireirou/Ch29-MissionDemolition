b844e2ae0fb952385a38c090c8f37c56906f5379
Claire Irou <c.iroudayassamy@gmail.com>
2019-09-17 14:26:19 -0400

    FollowCam
    The camera now follows the projectile as it flies through the air.

--- Assets/__Scripts/Slingshot.cs
+++ Assets/__Scripts/Slingshot.cs
@@ -93,6 +93,7 @@ public class Slingshot : MonoBehaviour {
             aimingMode = false;
             projectileRigidbody.isKinematic = false;
             projectileRigidbody.velocity = -mouseDelta * velocityMult;
+            FollowCam.POI = projectile;
             projectile = null;
             MissionDemolition.ShotFired();
             ProjectileLine.S.poi = projectile;
